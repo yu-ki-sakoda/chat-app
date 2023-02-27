97596672034777ae2b41bb3cacbd541fb8774d0c
yuki-sakoda <yuk104sako@icloud.com>
2023-02-27 23:46:44 +0900

    commit

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,15 @@ import "firebase/compat/auth";
 import "firebase/compat/firestore";
 
 const firebaseApp = firebase.initializeApp({
-  apiKey: process.env.PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.PUBLIC_FIREBASE_APP_ID
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 });
+
+console.log(firebaseApp.apiKey);
 
 const auth = firebase.auth();
 
