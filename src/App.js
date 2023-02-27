bc0810377536a29145edbfd4b1a981ff0a33c730
yuki-sakoda <yuk104sako@icloud.com>
2023-02-27 22:47:25 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import "./App.css";
 import { Line } from './components/Line';
 import { SignIn } from "./components/SignIn";
-import  auth  from './firebase';
+import { auth } from './firebase';
 import {useAuthState} from "react-firebase-hooks/auth"
 
 
